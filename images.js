8556a5804b3c03b04155fda3a0a76db721a5d883
samimyag <yagana.samim@mail.utoronto.ca>
2025-04-21 10:00:47 -0400

    Create chinesepictographsnegative.js

---
+++ images.js
@@ -0,0 +1,18 @@
+define(['pipAPI'], function(APIConstructor) {
+  var API = new APIConstructor();
+
+  return {
+    primeCats: [
+      {image: 'https://raw.githubusercontent.com/samimyag/chinesepictographsnegative/main/Crushed.jpg'},
+      {image: 'https://raw.githubusercontent.com/samimyag/chinesepictographsnegative/main/Hate.jpg'},
+      {image: 'https://raw.githubusercontent.com/samimyag/chinesepictographsnegative/main/Poison.jpg'},
+      {image: 'https://raw.githubusercontent.com/samimyag/chinesepictographsnegative/main/Pus.jpg'},
+      {image: 'https://raw.githubusercontent.com/samimyag/chinesepictographsnegative/main/Rotten.jpg'},
+      {image: 'https://raw.githubusercontent.com/samimyag/chinesepictographsnegative/main/Scalding.jpg'},
+      {image: 'https://raw.githubusercontent.com/samimyag/chinesepictographsnegative/main/Stink.jpg'},
+      {image: 'https://raw.githubusercontent.com/samimyag/chinesepictographsnegative/main/Thief.jpg'},
+      {image: 'https://raw.githubusercontent.com/samimyag/chinesepictographsnegative/main/Tomb.jpg'},
+      {image: 'https://raw.githubusercontent.com/samimyag/chinesepictographsnegative/main/Tumor.jpg'}
+    ]
+  };
+});
