df30eaf0c21de3a86a317f7f5a461450ef349d5e
samimyag <yagana.samim@mail.utoronto.ca>
2025-04-21 12:15:42 -0400

    Update images.js

--- images.js
+++ images.js
@@ -1,18 +1,35 @@
 define(['pipAPI'], function(APIConstructor) {
-  var API = new APIConstructor();
+   var API = new APIConstructor();
 
-  return {
-    primeCats: [
-      {image: 'https://raw.githubusercontent.com/samimyag/chinesepictographsnegative/main/Crushed.jpg'},
-      {image: 'https://raw.githubusercontent.com/samimyag/chinesepictographsnegative/main/Hate.jpg'},
-      {image: 'https://raw.githubusercontent.com/samimyag/chinesepictographsnegative/main/Poison.jpg'},
-      {image: 'https://raw.githubusercontent.com/samimyag/chinesepictographsnegative/main/Pus.jpg'},
-      {image: 'https://raw.githubusercontent.com/samimyag/chinesepictographsnegative/main/Rotten.jpg'},
-      {image: 'https://raw.githubusercontent.com/samimyag/chinesepictographsnegative/main/Scalding.jpg'},
-      {image: 'https://raw.githubusercontent.com/samimyag/chinesepictographsnegative/main/Stink.jpg'},
-      {image: 'https://raw.githubusercontent.com/samimyag/chinesepictographsnegative/main/Thief.jpg'},
-      {image: 'https://raw.githubusercontent.com/samimyag/chinesepictographsnegative/main/Tomb.jpg'},
-      {image: 'https://raw.githubusercontent.com/samimyag/chinesepictographsnegative/main/Tumor.jpg'}
-    ]
-  };
-});
+   // Function to shuffle the array
+   function shuffleArray(array) {
+       for (var i = array.length - 1; i > 0; i--) {
+           var j = Math.floor(Math.random() * (i + 1));
+           [array[i], array[j]] = [array[j], array[i]]; // Swap elements
+       }
+   }
+
+   // List of all the images
+   var allImages = [
+       {image: 'https://raw.githubusercontent.com/samimyag/chinesepictographsnegative/main/Crushed.jpg', label: 'Crushed'},
+       {image: 'https://raw.githubusercontent.com/samimyag/chinesepictographsnegative/main/Hate.jpg', label: 'Hate'},
+       {image: 'https://raw.githubusercontent.com/samimyag/chinesepictographsnegative/main/Poison.jpg', label: 'Poison'},
+       {image: 'https://raw.githubusercontent.com/samimyag/chinesepictographsnegative/main/Pus.jpg', label: 'Pus'},
+       {image: 'https://raw.githubusercontent.com/samimyag/chinesepictographsnegative/main/Rotten.jpg', label: 'Rotten'},
+       {image: 'https://raw.githubusercontent.com/samimyag/chinesepictographsnegative/main/Scalding.jpg', label: 'Scalding'},
+       {image: 'https://raw.githubusercontent.com/samimyag/chinesepictographsnegative/main/Stink.jpg', label: 'Stink'},
+       {image: 'https://raw.githubusercontent.com/samimyag/chinesepictographsnegative/main/Thief.jpg', label: 'Thief'},
+       {image: 'https://raw.githubusercontent.com/samimyag/chinesepictographsnegative/main/Tomb.jpg', label: 'Tomb'},
+       {image: 'https://raw.githubusercontent.com/samimyag/chinesepictographsnegative/main/Tumor.jpg', label: 'Tumor'}
+   ];
+
+   // Shuffle the array to randomize the order
+   shuffleArray(allImages);
+
+   // Select the first 6 images after shuffling
+   var selectedImages = allImages.slice(0, 6);
+
+   return {
+     primeCats: selectedImages
+   };
+ });
